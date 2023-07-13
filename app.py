bf3ff268680fc1d015d5f4f755bee9de6096545c
Javier Justin <jav.justin2004.jj@gmail.com>
2023-07-13 11:10:41 +0700

    added summarizer

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+import tkinter as tk
+import nltk
+from textblob import TextBlob
+from newspaper import Article
+
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
 
-def home():
-    return render_template("index.html")
+# def home():
+#     return render_template("index.html")
+
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       urlLink = request.form.get("url")
+       nltk.download('punkt')
+       article = Article(urlLink)
+       article.download()
+       article.parse()
+       article.nlp()
+       return "This is your summary <br></br>" + article.summary
+    return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
