6e020435da18423c956700a0d6a3303b6bb086f3
Javier Justin <jav.justin2004.jj@gmail.com>
2023-07-13 15:53:30 +0700

    connected index page(home) to text page

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ from newspaper import Article
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/', methods =["GET", "POST"])
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-def gfg():
+@app.route('/text', methods =["GET", "POST"])
+def text():
     if request.method == "POST":
        urlLink = request.form.get("url")
        nltk.download('punkt')
@@ -17,7 +20,9 @@ def gfg():
        article.parse()
        article.nlp()
        return "Title : " + article.title + "<br>" + "Authors : " + str(article.authors) + "<br>" + "Publication Date : " + str(article.publish_date) + "<br> </br>" + "Summary : <br>" + article.summary
-    return render_template("index.html")
+    return render_template("text.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
